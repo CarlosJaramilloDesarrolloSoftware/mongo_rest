bd3031c6da3093e29430caaea93fc6bb1b4451e4
CarlosJaramilloDesarrollo <docentecesde2019@gmail.com>
2019-10-24 18:56:07 -0500

    Creacion de carpetas models y controllers, model básico sin exportar

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
