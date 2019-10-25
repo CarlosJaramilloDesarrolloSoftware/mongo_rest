7e9acd14399b387ab9da3ba398f5beac54df7a0b
CarlosJaramilloDesarrollo <docentecesde2019@gmail.com>
2019-10-24 19:36:26 -0500

    Exports en Model y Funciones base, lista, guardar

--- server/server.js
+++ server/server.js
@@ -8,37 +8,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
  
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.json({
-      'mensaje' : 'Bienvenido al API de NODEJS + MONGODB + ROBO 3T'
-    })
-});
-
-app.get('/productos', function (req, res) {
-    res.json({
-        'data' : 'Aquí van los productos'
-    })
-});
-
-app.get('/producto/:id', function (req, res) {
-    res.json({
-        'data' : `Aquí recibí el ID para mostrar un producto. ID: ${req.params.id}`
-    })
-});
-
-app.post('/producto', function (req, res) {
-    let datos = req.body;
-
-    if(datos.nombre == undefined || datos.marca == undefined || datos.color == undefined){
-        res.status(400).json({
-            "err" : "Datos incompletos"
-        });
-    }else{
-        res.json({
-            'data' : req.body
-        })
-    }
-});
 
 mongoose.connect('mongodb://localhost:27017/tienda', {
   useNewUrlParser: true,
